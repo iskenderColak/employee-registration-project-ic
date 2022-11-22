15b1da07e0ee47d21d950f93ff01b43378d71d17
iskenderColak <iclk84@gmail.com>
2022-11-22 19:50:37 +0300

    adds employee and stateList models in form

--- src/main/java/com/icolak/controller/EmployeeController.java
+++ src/main/java/com/icolak/controller/EmployeeController.java
@@ -1,6 +1,9 @@
 package com.icolak.controller;
 
+import com.icolak.bootstrap.DataGenerator;
+import com.icolak.model.Employee;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +12,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class EmployeeController {
 
     @GetMapping("/register")
-    public String createEmployee() {
+    public String createEmployee(Model model) {
+
+        model.addAttribute("employee", new Employee());
+        model.addAttribute("stateList", DataGenerator.getAllState());
+
         return "employee/employee-create";
     }
 }
