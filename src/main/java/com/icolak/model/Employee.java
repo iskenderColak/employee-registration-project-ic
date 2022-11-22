856696a4ff467e603828a4937596775d98f2d8ed
iskenderColak <iclk84@gmail.com>
2022-11-22 17:40:43 +0300

    Employee - Employee Controller

---
+++ src/main/java/com/icolak/model/Employee.java
@@ -0,0 +1,25 @@
+package com.icolak.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+
+    private String firstName;
+    private String lastName;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate birthday;
+    private String email;
+    private String password;
+    private String address;
+    private String address2;
+    private String city;
+    private String state;
+    private String zipCode;
+}
