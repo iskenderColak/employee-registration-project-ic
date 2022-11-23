459c7e047fd088f49f0898e64b99f41125ffd704
iskenderColak <iclk84@gmail.com>
2022-11-23 10:50:43 +0300

    adds backend validation

--- src/main/java/com/icolak/controller/EmployeeController.java
+++ src/main/java/com/icolak/controller/EmployeeController.java
@@ -5,10 +5,13 @@ import com.icolak.model.Employee;
 import com.icolak.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/employee")
@@ -30,7 +33,12 @@ public class EmployeeController {
     }
 
     @PostMapping("/insert")
-    public String insertEmployee(@ModelAttribute("employee") Employee employee) {
+    public String insertEmployee(@ModelAttribute("employee") @Valid Employee employee, BindingResult bindingResult,Model model) {
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("stateList", DataGenerator.getAllState());
+            return "employee/employee-create";
+        }
 
         employeeService.saveEmployee(employee);
 
--- src/main/java/com/icolak/model/Employee.java
+++ src/main/java/com/icolak/model/Employee.java
@@ -5,12 +5,28 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Employee {
 
+//    @NotNull        -->   Field shouldn't be null
+//    @NotEmpty       -->   Field shouldn't be ""
+//    @NotBlank       -->   Field shouldn't be "     "  NotBlank covering all of them
+
+//    @NotNull        -->   @NotNull
+//    @NotEmpty       -->   @NotNull + @NotEmpty
+//    @NotBlank       -->   @NotNull + @NotEmpty + @NotBlank
+
+    @NotBlank
+    @NotEmpty
+    @NotNull
+    @Size(max = 12, min = 2)
     private String firstName;
     private String lastName;
     // Thymeleaf accepts yyyy-MM-dd which is different from LocalDate
